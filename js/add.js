3adf1ceb25522f187c1bad953ea2a8c9be68c0d9
maiconmartins <maicon292010@gmail.com>
2021-12-31 21:42:38 +0000

    add.js created

---
+++ js/add.js
@@ -0,0 +1,37 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("Nationality").value;
+    formData["empCode"] = document.getElementById("age").value;
+    formData["salary"] = document.getElementById("dream").value;
+    formData["city"] = document.getElementById("NoCountry").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("BucketList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
