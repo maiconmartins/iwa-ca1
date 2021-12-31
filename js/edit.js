28f23dd53bad6b7c962fb1bdde4c14c1ebc0e67f
maiconmartins <maicon292010@gmail.com>
2021-12-31 21:44:48 +0000

    edit.js created

---
+++ js/edit.js
@@ -0,0 +1,21 @@
+function resetForm() {
+    document.getElementById("Nationality").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("dream").value = "";
+    document.getElementById("NoCountry").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("Nationality").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("dream").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("NoCountry").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.Nationality;
+    selectedRow.cells[1].innerHTML = formData.age;
+    selectedRow.cells[2].innerHTML = formData.dream;
+    selectedRow.cells[3].innerHTML = formData.NoCountry;
+}
